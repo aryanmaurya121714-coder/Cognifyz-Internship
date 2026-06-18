fbb0322e24646f9e5d307602986513e01a1e3891
Aryan Maurya <aryanmauryam509@gmail.com>
2026-06-18 17:30:36 +0530

    Updated Tasks

--- level_1/Task_3_Student_Grade_Calculator.java
+++ level_1/Task_3_Student_Grade_Calculator.java
@@ -5,12 +5,16 @@ public class Task_3_Student_Grade_Calculator {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        System.out.print("Enter marks of 5 subjects: ");
-
+        System.out.print("Enter marks of 5 subjects\n ");
+        System.out.print("Enter marks of 1 subjects: ");
         int s1 = sc.nextInt();
+        System.out.print("\nEnter marks of 2 subjects: ");
         int s2 = sc.nextInt();
+        System.out.print("\nEnter marks of 3 subjects: ");
         int s3 = sc.nextInt();
+        System.out.print("\nEnter marks of 4 subjects: ");
         int s4 = sc.nextInt();
+        System.out.print("\nEnter marks of 5 subjects: ");
         int s5 = sc.nextInt();
 
         int total = s1 + s2 + s3 + s4 + s5;
